05fbabae012cfaee95f334e988d697e15bd6f58d
rragul <r.ragul0598@gmail.com>
2025-03-20 08:54:16 +0530

    #2, server initialized with Spring Boot

---
+++ server/src/test/java/com/ragul/attendancesystem/AttendancesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragul.attendancesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendancesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
